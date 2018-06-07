0c787f469621b9ced2aff66c5641a0b1b3548614
Italo Brenner Lauriano de Oliveira <italobrenner.mf@gmail.com>
2018-06-07 09:23:08 -0300

    Cliente, TipoCliente, telefones e enderecos

--- src/main/java/br/com/italobrenner/cursomc/CursomcApplication.java
+++ src/main/java/br/com/italobrenner/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.italobrenner.cursomc.domain.Categoria;
 import br.com.italobrenner.cursomc.domain.Cidade;
+import br.com.italobrenner.cursomc.domain.Cliente;
+import br.com.italobrenner.cursomc.domain.Endereco;
 import br.com.italobrenner.cursomc.domain.Estado;
 import br.com.italobrenner.cursomc.domain.Produto;
+import br.com.italobrenner.cursomc.domain.enums.TipoCliente;
 import br.com.italobrenner.cursomc.repositories.CategoriaRepository;
 import br.com.italobrenner.cursomc.repositories.CidadeRepository;
+import br.com.italobrenner.cursomc.repositories.ClienteRepository;
+import br.com.italobrenner.cursomc.repositories.EnderecoRepository;
 import br.com.italobrenner.cursomc.repositories.EstadoRepository;
 import br.com.italobrenner.cursomc.repositories.ProdutoRepository;
 
@@ -30,6 +35,12 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -51,6 +62,9 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
 		Estado est1 = new Estado(null, "Minas Gerais");
 		Estado est2 = new Estado(null, "São Paulo");
 		
@@ -61,9 +75,19 @@ public class CursomcApplication implements CommandLineRunner {
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "sala 800", "Centro", "38777012", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 	}
 }
