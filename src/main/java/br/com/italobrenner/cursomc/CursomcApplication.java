752886b2a14b66c026c81f0ba8bda5f5464154bd
Italo Brenner Lauriano de Oliveira <italobrenner.mf@gmail.com>
2018-06-08 09:21:07 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/br/com/italobrenner/cursomc/CursomcApplication.java
+++ src/main/java/br/com/italobrenner/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import br.com.italobrenner.cursomc.domain.Cidade;
 import br.com.italobrenner.cursomc.domain.Cliente;
 import br.com.italobrenner.cursomc.domain.Endereco;
 import br.com.italobrenner.cursomc.domain.Estado;
+import br.com.italobrenner.cursomc.domain.ItemPedido;
 import br.com.italobrenner.cursomc.domain.Pagamento;
 import br.com.italobrenner.cursomc.domain.PagamentoComBoleto;
 import br.com.italobrenner.cursomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import br.com.italobrenner.cursomc.repositories.CidadeRepository;
 import br.com.italobrenner.cursomc.repositories.ClienteRepository;
 import br.com.italobrenner.cursomc.repositories.EnderecoRepository;
 import br.com.italobrenner.cursomc.repositories.EstadoRepository;
+import br.com.italobrenner.cursomc.repositories.ItemPedidoRepository;
 import br.com.italobrenner.cursomc.repositories.PagamentoRepository;
 import br.com.italobrenner.cursomc.repositories.PedidoRepository;
 import br.com.italobrenner.cursomc.repositories.ProdutoRepository;
@@ -55,6 +57,9 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -119,5 +124,18 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0., 1, 2000.);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0., 2, 80.);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100., 1, 800.);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 	}
 }
--- src/main/java/br/com/italobrenner/cursomc/domain/Pedido.java
+++ src/main/java/br/com/italobrenner/cursomc/domain/Pedido.java
@@ -1,7 +1,11 @@
 package br.com.italobrenner.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -10,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -34,6 +39,9 @@ public class Pedido implements Serializable {
 	@JoinColumn(name="endereco_de_entrega_id")
 	private Endereco enderecoDeEntrega;
 	
+	@OneToMany(mappedBy="id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>();
+	
 	public Pedido() {
 	}
 
@@ -43,7 +51,15 @@ public class Pedido implements Serializable {
 		this.cliente = cliente;
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
-
+	
+	public List<Produto> getProdutos() {
+		List<Produto> lista = new ArrayList<>();
+		for (ItemPedido itemPedido : itens) {
+			lista.add(itemPedido.getProduto());
+		}
+		return lista;
+	}
+	
 	public Integer getId() {
 		return id;
 	}
@@ -82,6 +98,14 @@ public class Pedido implements Serializable {
 
 	public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
 		this.enderecoDeEntrega = enderecoDeEntrega;
+	}
+	
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
 	}
 
 	@Override
