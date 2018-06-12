dae37466d21e59ba28b1109270bbb6e168527dba
Italo Brenner Lauriano de Oliveira <italobrenner.mf@gmail.com>
2018-06-12 14:33:48 -0300

    Busca de produtos por nome e categorias

---
+++ src/main/java/br/com/italobrenner/cursomc/services/ProdutoService.java
@@ -0,0 +1,39 @@
+package br.com.italobrenner.cursomc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import br.com.italobrenner.cursomc.domain.Categoria;
+import br.com.italobrenner.cursomc.domain.Produto;
+import br.com.italobrenner.cursomc.repositories.CategoriaRepository;
+import br.com.italobrenner.cursomc.repositories.ProdutoRepository;
+import br.com.italobrenner.cursomc.services.exception.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public Produto find(Integer id) {
+		Optional<Produto> produto = produtoRepository.findById(id);
+		return produto.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName()));
+	}
+
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPages, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPages, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		return produtoRepository.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);
+	}
+	
+}
