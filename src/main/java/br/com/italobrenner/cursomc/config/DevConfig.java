2e8a6cb50b0ef224ac446cf07c9a4234e8788d8f
Italo Brenner Lauriano de Oliveira <italobrenner.mf@gmail.com>
2018-06-13 16:56:19 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/br/com/italobrenner/cursomc/config/DevConfig.java
+++ src/main/java/br/com/italobrenner/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import br.com.italobrenner.cursomc.services.DBService;
+import br.com.italobrenner.cursomc.services.EmailService;
+import br.com.italobrenner.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -27,6 +29,11 @@ public class DevConfig {
 		}
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 	
 }
